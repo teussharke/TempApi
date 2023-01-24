5032617a369548ad74c3a70a13135269a699b035
Mateus Oliveira <teussharke@gmail.com>
2023-01-24 19:54:54 -0300

    att

--- src/lib/fetchWeather.tsx
+++ src/lib/fetchWeather.tsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-
-
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '0c09398048e493ed8f8ef03cee646572';
 export const fetchWeather = async (query: string) => {
