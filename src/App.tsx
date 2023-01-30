01227f9c96df078fcfbed9af3ee67d1db2d33a7c
Mateus Oliveira <teussharke@gmail.com>
2023-01-30 09:47:40 -0300

    end

--- src/App.tsx
+++ src/App.tsx
@@ -40,42 +40,45 @@ export function App() {
   }
 
   return (
-    <div className='main-container'>
-      <input
-        type="text"
-        className="search"
-        placeholder="Digite a sua ciade"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={serch}
+    <>
 
-      />
+      <div className='main-container'>
+        <input
+          type="text"
+          className="search"
+          placeholder="Digite a sua ciade"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={serch}
+
+        />
 
 
 
-      {weather.main && (
-        <div className='city'>
-          <h2 className='city-name'>
-            <span>
-              {weather.name}
-            </span>
-            <sup>
-              {weather.sys.country}
-            </sup>
-          </h2>
-          <div className='city-temp'>
-            {Math.round(weather.main.temp)}
-            <sup>
-              &deg;C
-            </sup>
+        {weather.main && (
+          <div className='city'>
+            <h2 className='city-name'>
+              <span>
+                {weather.name}
+              </span>
+              <sup>
+                {weather.sys.country}
+              </sup>
+            </h2>
+            <div className='city-temp'>
+              {Math.round(weather.main.temp)}
+              <sup>
+                &deg;C
+              </sup>
+            </div>
+            <div className='info'>
+              <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+              <p>{weather.weather[0].description}</p>
+            </div>
           </div>
-          <div className='info'>
-            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-            <p>{weather.weather[0].description}</p>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   )
 }
 
--- src/lib/fetchWeather.tsx
+++ src/lib/fetchWeather.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const URL = 'https://api.openweathermap.org/data/2.5/weather?&lang=pt_br';
 const API_KEY = '0c09398048e493ed8f8ef03cee646572';
 export const fetchWeather = async (query: string) => {
   const response = await axios.get(URL, {
